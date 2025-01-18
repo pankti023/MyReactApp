403fbb5ba51e33676a879294a2063c909a8eb3d9
pankti023 <59198223+pankti023@users.noreply.github.com>
2025-01-18 16:15:11 -0500

    Add files via upload

---
+++ build/precache-manifest.73a4a717f3d2f884e5f95577a3e6dc2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab2400854bf52a7147c2e62e5667845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecaa489daf68a65e22ff",
+    "url": "/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "804b52b7d94867ef40a8",
+    "url": "/static/css/main.8013c1d5.chunk.css"
+  },
+  {
+    "revision": "ecaa489daf68a65e22ff",
+    "url": "/static/js/2.19c79e46.chunk.js"
+  },
+  {
+    "revision": "00d06645ba5f6a26cd1a8d5ea324088d",
+    "url": "/static/js/2.19c79e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35db66ba18800b141f6f",
+    "url": "/static/js/3.b810ec05.chunk.js"
+  },
+  {
+    "revision": "804b52b7d94867ef40a8",
+    "url": "/static/js/main.34e00ecf.chunk.js"
+  },
+  {
+    "revision": "e4d4828e38e6bc77780f",
+    "url": "/static/js/runtime-main.dd039199.js"
+  }
+]);
